464bb488f377ad9417070c40871d435a4a4684da
Xavier Buckner <xavier@Xaviers-MacBook-Pro.local>
2022-12-24 21:41:22 -0600

    MVP Reached

--- Understanding-Questions.md
+++ Understanding-Questions.md
@@ -1,7 +1,6 @@
 # Understanding Questions:
 1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
 * The user presses the 1 button.
-* 
-...
-
+* The type is sent through the reducer function
+* It matches the type with the case in the reduce function
 * TotalDisplay shows the total plus 1.
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
-
-import './App.css';
-
+import React, {useReducer} from 'react';
 import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
+import {reducer, initialState} from "../reducers/index"
+import { ADD_MEMORY ,CLEAR_DISPLAY, ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION } from './../actions';
+import './App.css';
+
+
+
+
+
 
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  const Calculate = (num)=>{
+    return {type: 'APPLY_NUMBER', payload: num }
+  }
+
+  const opperation = (operand) =>{
+    return({type: 'CHANGE_OPERATION', payload: operand})
+  }
+
+  console.log(state)
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
@@ -16,44 +33,44 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {state.operation}</span>
+              <span id="memory"><b>Memory:</b> {state.memory} </span>
             </div>
             
             <div className="row">
-              <CalcButton value={"M+"}/>
-              <CalcButton value={"MR"}/>
-              <CalcButton value={"MC"}/>
+              <CalcButton  value={"M+"} onClick={()=> dispatch( {type: "ADD_MEMORY"})}/>
+              <CalcButton  value={"MR"} onClick={() => dispatch(Calculate(state.memory))}/>
+              <CalcButton  value={"MC"} onClick={() => dispatch({ type: "CLEAR_MEMORY" })}/>
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton  value={1} onClick={() => dispatch(Calculate(1))} />
+              <CalcButton  value={2} onClick={() => dispatch(Calculate(2))}/>
+              <CalcButton  value={3} onClick={() => dispatch(Calculate(3))}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton  value={4} onClick={() => dispatch(Calculate(4))}/>
+              <CalcButton  value={5} onClick={() => dispatch(Calculate(5))}/>
+              <CalcButton  value={6} onClick={() => dispatch(Calculate(6))}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton  value={7} onClick={() => dispatch(Calculate(7))}/>
+              <CalcButton  value={8} onClick={() => dispatch(Calculate(8))}/>
+              <CalcButton  value={9} onClick={() => dispatch(Calculate(9))}/>
             </div>
 
             <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
+              <CalcButton  value={"+"} onClick={()=> dispatch(opperation("+"))}/>
+              <CalcButton  value={"*"} onClick={()=> dispatch(opperation("*"))}/>
+              <CalcButton  value={"-"} onClick={()=> dispatch(opperation("-"))}/>
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+              <CalcButton   value={"CE"} onClick={() => dispatch({ type: "CLEAR_DISPLAY" })}/>
             </div>
 
           </form>
